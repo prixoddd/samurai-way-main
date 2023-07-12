5873131dbe0fdb24152da92fb6ba8a11cc2c6903
nikita <nikita.derjugin@gmail.com>
2023-07-12 18:10:54 +0300

    lesson 75 done

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -4,6 +4,7 @@ import dialogsReducer from './dialogs-reducer';
 import {usersReducer} from './users-reducer';
 import {authReducer} from './auth-reducer';
 import thunkMiddleware from 'redux-thunk';
+import {reducer as formReducer} from 'redux-form'
 
 type ReducersType = typeof rootReducer
 export type AppStateType = ReturnType<ReducersType>
@@ -13,7 +14,8 @@ export const rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 let store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
