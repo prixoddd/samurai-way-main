7af1667bc62bc93416564da1a883e4b61163235e
nikita <nikita.derjugin@gmail.com>
2023-08-13 13:06:46 +0300

    lesson 83 done reselect used in users-selectors.ts

--- src/redux/users-selectors.ts
+++ src/redux/users-selectors.ts
@@ -1,8 +1,17 @@
 import { AppStateType } from "redux/redux-store"
+import { createSelector } from "reselect"
 
 export const getUsers = (state: AppStateType) => {
     return state.usersPage.users
 }
+
+// export const getUsersSelector = (state: AppStateType) => {
+//     return getUsers(state).filter((u) => true)
+// }
+
+export const getUsersSuper = createSelector(getUsers, (users) => {
+    return users.filter((u) => u.id > 1)
+})
 
 export const getPageSize = (state: AppStateType) => {
     return state.usersPage.pageSize
