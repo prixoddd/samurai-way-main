eae14ae247594a9a35601e7d8b801cfa59574f82
nikita <nikita.derjugin@gmail.com>
2023-09-13 17:34:31 +0300

    95-2 github pages /// gh-pages ok BrowserRouter fix

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,7 @@ let AppContainer = compose<ComponentType>(withRouter, connect(mapStateToProps, {
 
 const SamuraiJsApp = () => {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Provider store={store}>
                 <AppContainer />
             </Provider>
